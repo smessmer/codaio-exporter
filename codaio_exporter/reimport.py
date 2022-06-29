0691b94254a742db553870d501b4b02296b4cfb4
Sebastian Messmer <mail@smessmer.de>
2022-06-28 22:44:06 -0700

    Fix loading

--- codaio_exporter/reimport.py
+++ codaio_exporter/reimport.py
@@ -7,9 +7,8 @@ from codaio_exporter.table import Table
 
 async def reimport_doc(api_token: str, source_path: str, dest_doc_id: str, progress_callback: Optional[ProgressCallback] = None) -> None:
     tables_path = os.path.join(source_path, "tables/table")
-    for table_path in os.listdir(tables_path):
-        with open(os.path.join(table_path, "table.json"), 'r') as file:
+    for table_dir in os.listdir(tables_path):
+        with open(os.path.join(tables_path, table_dir, "table.json"), 'r') as file:
             json = file.read()
             table = Table.from_json(json)
             print(table)
-
