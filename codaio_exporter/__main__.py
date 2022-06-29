39f0e05cb8ab045fe32ae94bf4666a7d7537b770
Sebastian Messmer <mail@smessmer.de>
2022-06-28 22:37:02 -0700

    Start adding a reimport command

--- codaio_exporter/__main__.py
+++ codaio_exporter/__main__.py
@@ -2,9 +2,11 @@ import argparse
 import asyncio
 import logging
 import curses
-from typing import Dict, Optional
+from typing import Dict, Optional, NoReturn
 
 from codaio_exporter.export import export_all_docs, export_doc
+from codaio_exporter.reimport import reimport_doc
+
 
 class ProgressDisplay:
     def __init__(self) -> None:
@@ -32,25 +34,51 @@ async def async_main() -> None:
 
     parser = argparse.ArgumentParser(description='Export tables from coda.io')
     parser.add_argument('--api-token', type=str)
-    parser.add_argument('--dest-dir', type=str, help="Destination directory to export to")
-    parser.add_argument('--doc-id', type=str, help="Limit export to the tables in the document with the given id")
+    subparsers = parser.add_subparsers(title='subcommands')
+
+    parser_export = subparsers.add_parser('export', help="Export tables from one or more coda.io documents")
+    parser_export.add_argument('--dest-dir', type=str, help="Destination directory to export to")
+    parser_export.add_argument('--src-doc-id', type=str, help="Limit export to the tables in the document with the given id")
+    parser_export.set_defaults(func=main_export)
+
+    parser_reimport = subparsers.add_parser('reimport', help="Reimport previously exported tables into a coda.io document")
+    parser_reimport.add_argument('--src-dir', type=str, help="Directory with the exported document")
+    parser_reimport.add_argument('--dest-doc-id', type=str, help="The coda.io id of the document to import tables into")
+    parser_reimport.set_defaults(func=main_reimport)
 
     args = parser.parse_args()
 
-    progress_display = ProgressDisplay()
-
     if args.api_token is None:
-        print("Please specify the --api-token parameter")
-        exit(1)
+        error_exit("Please specify the --api-token parameter")
 
+    await args.func(args)
+
+async def main_export(args: argparse.Namespace) -> None:
     if args.dest_dir is None:
-        print("Please specify the --dest-dir parameter")
-        exit(1)
+        error_exit("Please specify the --dest-dir parameter")
+
+    progress_display = ProgressDisplay()
     
-    if args.doc_id is None:
+    if args.src_doc_id is None:
         await export_all_docs(args.api_token, args.dest_dir, progress_display.progress_callback)
     else:
-        await export_doc(args.api_token, args.dest_dir, args.doc_id, progress_display.progress_callback)
+        await export_doc(args.api_token, args.dest_dir, args.src_doc_id, progress_display.progress_callback)
+
+async def main_reimport(args: argparse.Namespace) -> None:
+    if args.src_dir is None:
+        error_exit("Please specify the --src-dir argument")
+
+    if args.dest_doc_id is None:
+        error_exit("Please specify the --dest-doc-id argument")
+
+    progress_display = ProgressDisplay()
+    
+    await reimport_doc(args.api_token, args.src_dir, args.dest_doc_id, progress_display.progress_callback)
+    
+    
+def error_exit(msg: str) -> NoReturn:
+    print(msg)
+    exit(1)
 
 def main() -> None:
     asyncio.run(async_main())
--- codaio_exporter/reimport.py
+++ codaio_exporter/reimport.py
@@ -1,6 +1,15 @@
+import os
 from typing import Optional
+
 from codaio_exporter.progress import ProgressCallback
+from codaio_exporter.table import Table
 
 
 async def reimport_doc(api_token: str, source_path: str, dest_doc_id: str, progress_callback: Optional[ProgressCallback] = None) -> None:
-    pass
+    tables_path = os.path.join(source_path, "tables/table")
+    for table_path in os.listdir(tables_path):
+        with open(os.path.join(table_path, "table.json"), 'r') as file:
+            json = file.read()
+            table = Table.from_json(json)
+            print(table)
+
